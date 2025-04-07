ebb9bf90eae6081ec57b43777728732496e399a3
Sanapol <junrey@list.ru>
2025-04-07 19:13:26 +0300

    add AppApplication.java, application.yml, Dockerfile, WelcomeController.java

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,37 @@
 plugins {
+    application
     id("java")
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
+application {
+    mainClass.set("hexlet.code.AppApplication")
+}
+
+
 repositories {
     mavenCentral()
+    maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+    runtimeOnly("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("net.datafaker:datafaker:2.0.2")
+    implementation("org.instancio:instancio-junit:3.3.1")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
