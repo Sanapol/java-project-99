509e0c7b61149ba0285e19e51533266ba5e7660d
Sanapol <junrey@list.ru>
2025-04-17 09:15:54 +0300

    update AuthRequest.java, DataInitializer.java

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -2,7 +2,9 @@ package hexlet.code.component;
 
 import hexlet.code.dto.UserCreateDto;
 import hexlet.code.mapper.UserMapper;
+import hexlet.code.model.TaskStatus;
 import hexlet.code.model.User;
+import hexlet.code.repository.TaskStatusRepository;
 import hexlet.code.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +22,11 @@ public class DataInitializer implements ApplicationRunner {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
     @Override
-    public final void run(ApplicationArguments args) {
+    public void run(ApplicationArguments args) {
         String email = "hexlet@example.com";
         String password = "qwerty";
         String firstName = "admin";
@@ -33,5 +38,44 @@ public class DataInitializer implements ApplicationRunner {
         userData.setLastName(lastName);
         User user = userMapper.map(userData);
         userRepository.save(user);
+        createTaskStatus();
+    }
+
+    private void createTaskStatus() {
+
+        String draft = "Draft";
+        String draftSlug = "draft";
+        TaskStatus statusDraft = new TaskStatus();
+        statusDraft.setName(draft);
+        statusDraft.setSlug(draftSlug);
+        taskStatusRepository.save(statusDraft);
+
+        String toReview = "ToReview";
+        String toReviewSlug = "to_review";
+        TaskStatus statusToReview = new TaskStatus();
+        statusToReview.setName(toReview);
+        statusToReview.setSlug(toReviewSlug);
+        taskStatusRepository.save(statusToReview);
+
+        String toBeFixed = "ToBeFixed";
+        String toBeFixedSlug = "to_be_fixed";
+        TaskStatus statusToBeFixed = new TaskStatus();
+        statusToBeFixed.setName(toBeFixed);
+        statusToBeFixed.setSlug(toBeFixedSlug);
+        taskStatusRepository.save(statusToBeFixed);
+
+        String toPublish = "ToPublish";
+        String toPublishSlug = "to_publish";
+        TaskStatus statusToPublish = new TaskStatus();
+        statusToPublish.setName(toPublish);
+        statusToPublish.setSlug(toPublishSlug);
+        taskStatusRepository.save(statusToPublish);
+
+        String published = "Published";
+        String publishedSlug = "published";
+        TaskStatus statusPublished = new TaskStatus();
+        statusPublished.setName(published);
+        statusPublished.setSlug(publishedSlug);
+        taskStatusRepository.save(statusPublished);
     }
 }
