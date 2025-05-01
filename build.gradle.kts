e7a66bab50b8478eee6e8bae4b4eb04fe46dcd9e
Sanapol <junrey@list.ru>
2025-05-01 18:35:25 +0300

    update build.yml, build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,10 @@ sentry {
     authToken = System.getenv("SENTRY_AUTH_TOKEN")
 }
 
+tasks.sentryBundleSourcesJava {
+    enabled = System.getenv("SENTRY_AUTH_TOKEN") != null
+}
+
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
